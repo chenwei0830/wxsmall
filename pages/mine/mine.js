44d3ad71aea3671f6d874f241799d309386133d3
LI MIN <min@art.com>
2017-10-14 18:29:30 +0800

    tijiao

---
+++ pages/mine/mine.js
@@ -0,0 +1,52 @@
+var base64 = require("../../images/base64");
+//获取应用实例
+const app = getApp()
+
+Page({
+  data:{
+      icon20: base64.icon20,
+      icon60: base64.icon60,
+      headImage:'../../images/head.png',
+      userName:'李敏',
+      list: [
+        {
+          id: 'form',
+          name: '表单',
+          open: false,
+          pages: ['button', 'list', 'input', 'slider', 'uploader']
+        },
+        {
+          id: 'widget',
+          name: '基础组件',
+          open: false,
+          pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loadmore', 'panel', 'preview', 'progress']
+        },
+        {
+          id: 'feedback',
+          name: '操作反馈',
+          open: false,
+          pages: ['actionsheet', 'dialog', 'msg', 'picker', 'toast']
+        },
+        {
+          id: 'nav',
+          name: '导航相关',
+          open: false,
+          pages: ['navbar', 'tabbar']
+        },
+        {
+          id: 'search',
+          name: '搜索相关',
+          open: false,
+          pages: ['searchbar']
+        }
+      ]
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+  }
+})
